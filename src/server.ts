60cf8cebacfdf9852888e1b32a5760b06acda718
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-06-23 15:38:36 -0300

    Add route create appointment

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express, { request, response } from 'express'
+import express, { json } from 'express'
+import routes from './routes'
+
 
 const app = express()
 
-app.get('/', (request, response) =>{
 
-    return response.send('online')
- } )
+app.use(json())
+app.use(routes)
 
 
- app.listen(3333,()=>console.log('Server online on port 3333'))
+app.listen(3333,()=>console.log('Server online on port 3333'))
