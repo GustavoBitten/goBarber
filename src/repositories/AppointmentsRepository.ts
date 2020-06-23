2fd815acd028dc989e02f12bf022f68cbc6874ec
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-06-23 17:06:53 -0300

    Implement DIO

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,26 +1,36 @@
-import {isEqual} from 'date-fns'
+import {
+    isEqual
+} from 'date-fns'
 import Appointment from '../models/Appointment'
+
+interface CreateAppointmentDTO {
+    provider: string
+    date: Date
+}
 
 class AppointmentsRepository {
     private appointments: Appointment[]
 
-    constructor(){
+    constructor() {
         this.appointments = []
     }
 
-    public all(): Appointment[]{
+    public all(): Appointment[] {
         return this.appointments
     }
 
-    public findByDate(date: Date): Appointment | null{
+    public findByDate(date: Date): Appointment | null {
         const findAppointment = this.appointments.find(appointment =>
-            isEqual(date,appointment.date)    
+            isEqual(date, appointment.date)
         )
         return findAppointment || null
     }
 
-    public create(provider: string, date: Date ): Appointment {
-        const appointment = new Appointment(provider,date)
+    public create({
+        date,
+        provider
+    }: CreateAppointmentDTO): Appointment {
+        const appointment = new Appointment({provider, date})
 
         this.appointments.push(appointment)
 
