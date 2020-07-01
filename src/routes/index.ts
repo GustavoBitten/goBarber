375da089f891699792e5672196c87e22668617b9
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-07-01 03:33:46 -0300

    Add route for static images

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import sessionsRoutes from './sessions.routes'
 const routes = Router()
 
 routes.use('/appointments',appointmentsRouter)
+
 routes.use('/users',usersRoutes)
 routes.use('/sessions',sessionsRoutes)
 
