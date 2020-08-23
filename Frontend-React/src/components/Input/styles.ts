63b4c3096311f65fc5082db80bb74396f3cf65c8
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-08-23 16:58:40 -0300

    Usability for users

--- Frontend-React/src/components/Input/styles.ts
+++ Frontend-React/src/components/Input/styles.ts
@@ -1,17 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Conteiner = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Conteiner = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
   padding: 16px;
   width: 100%;
-  align-items: color-interpolation-filters;
+  align-items: center;
   color: #666360;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
 
   input {
     flex: 1;
