504061aac70cb0590326535ffd99c827bf4e176a
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-19 11:12:27 -0300

    config test to appointmentsRepository

---
+++ Backend-API/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class FakeAppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentsDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default FakeAppointmentsRepository;
--- Backend-API/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ Backend-API/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('sum 2', () => {
-  expect(1 + 3).toBe(4);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '1232132',
+    });
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1232132');
+  });
+
+  it('should not be able to create tow appointment on the same time', () => {
+    expect('').toBe('');
+  });
 });
