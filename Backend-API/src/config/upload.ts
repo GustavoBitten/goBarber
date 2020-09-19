93b7cb03d61ad6b0eedbff49856c535f5f9c74d3
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-19 15:37:25 -0300

    Add StorageProvider

--- Backend-API/src/config/upload.ts
+++ Backend-API/src/config/upload.ts
@@ -1,22 +1,22 @@
-import multer from "multer";
-import path from "path";
-import crypto from 'crypto'
+import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
 
-const tmpFolder = path.resolve(__dirname,'..','..','tmp')
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 const uploadConfig = {
-    directory: tmpFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file , callback ){
-            const fileHash = crypto.randomBytes(10).toString('hex')
-            const filename = `${fileHash}-${file.originalname}`
-            
-            return callback(null,filename)
-            
-        }
-    })
-}
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const filename = `${fileHash}-${file.originalname}`;
 
-export default uploadConfig
+      return callback(null, filename);
+    },
+  }),
+};
+
+export default uploadConfig;
