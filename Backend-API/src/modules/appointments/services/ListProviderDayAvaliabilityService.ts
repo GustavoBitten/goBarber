ec1906a4c87c4adbd14f4da4e6fd49e0085e897c
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-22 23:06:40 -0300

    Delete old times for appointments

--- Backend-API/src/modules/appointments/services/ListProviderDayAvaliabilityService.ts
+++ Backend-API/src/modules/appointments/services/ListProviderDayAvaliabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequestDTO {
@@ -42,12 +42,19 @@ class ListProviderDayAvaliabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHours = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
-      return { hour, available: !hasAppointmentInHours };
+      const compareDate = new Date(year, month - 1, day, hour);
+
+      return {
+        hour,
+        available: !hasAppointmentInHours && isAfter(compareDate, currentDate),
+      };
     });
 
     return availability;
