821b1cf87b482765b3e5b24dd42caa1a5cca729b
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-10-14 18:01:53 -0300

    Validade data

--- Backend-API/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Backend-API/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
@@ -10,12 +11,16 @@ appointmentsRouter.use(ensureAuthenticated);
 const appointmentsControllers = new AppointmentsController();
 const providerAppointmentsController = new ProviderAppointmentsController();
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', appointmentsControllers.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsControllers.create,
+);
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
--- Backend-API/src/shared/infra/http/server.ts
+++ Backend-API/src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import '@shared/infra/typeorm/index';
 import '@shared/container';
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
